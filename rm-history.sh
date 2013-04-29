ac29963676b994fa1d15900e3f1491f428742e71
Tom Vincent <git@tlvince.com>
2013-04-29 21:49:08 +0800

    Remove whitespace

--- rm-history.sh
+++ rm-history.sh
@@ -4,17 +4,17 @@
 # Copyright 2012 Tom Vincent <http://tlvince.com/contact/>
 
 error() { echo "error: $1" >&2 && exit 1; }
- 
+
 [ -d ".git" ] || error "must be ran from the root of a git repository"
 
 files="$@"
- 
+
 # Remove all paths passed as arguments from the history of the repo
 git filter-branch --tag-name-filter cat --index-filter \
     "git rm -rfq --cached --ignore-unmatch $files" --prune-empty -- --all --tags
 
 [ $? -ne 0 ] && exit 1
- 
+
 # Remove the temporary history git-filter-branch leaves behind
 rm -rf .git/refs/original/
 git reflog expire --all
