14875332e269db5919de37ecfd559376163c801e
Tom Vincent <git@tlvince.com>
2013-06-26 17:40:46 +0800

    Remove the ellipsis, fold at 50

--- aliases/add-commit.sh
+++ aliases/add-commit.sh
@@ -2,4 +2,4 @@
 # Adds and commits the given files.
 # © 2013 Tom Vincent <http://tlvince.com/contact>
 
-git add "$@"; git commit "$@" -m "$(echo "Added $@" | fold -sw 49 | head -n1)…"
+git add "$@"; git commit "$@" -m "$(echo "Added $@" | fold -sw 50 | head -n1)"
