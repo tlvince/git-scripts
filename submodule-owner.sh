5cc5d90d3aee04b2a3323236005a7e2dc96ce2af
Tom Vincent <git@tlvince.com>
2013-10-28 17:04:47 +0000

    Prefer gh: shorthand over ssh://

--- submodule-owner.sh
+++ submodule-owner.sh
@@ -3,4 +3,4 @@
 # © 2013 Tom Vincent <http://tlvince.com/contact>
 
 origin="$(git config --get remote.origin.url)"
-[ ${origin:0:6} = "ssh://" ] || [ ${origin:0:4} = "git@" ] && "$@" || exit 0
+[ ${origin:0:3} = "gh:" ] || [ ${origin:0:4} = "git@" ] && "$@" || exit 0
